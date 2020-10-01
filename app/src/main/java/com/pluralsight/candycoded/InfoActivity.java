ff2a1011355abf2f3e48591e4ce081d73c5418cf
meemcoder <62311464+meemcoder@users.noreply.github.com>
2020-10-01 17:22:36 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -37,6 +37,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     // ***
-    // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri uriPhoneDial = Uri.parse("tel:0123456789");
+        phoneIntent.setData(uriPhoneDial);
+        startActivity(phoneIntent);
+    }
     // ***
 }
